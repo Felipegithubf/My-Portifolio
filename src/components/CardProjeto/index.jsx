9baf290547de0115011121853579c5711c411039
Felipegithubf <felipecarvalho5055@gmail.com>
2024-06-24 01:39:16 -0300

    alteração

--- src/components/CardProjeto/index.jsx
+++ src/components/CardProjeto/index.jsx
@@ -1,67 +1,49 @@
-import ImgProject from '../../assets/1.png'
+import React from 'react';
+import ImgProject from '../../assets/1.png';
 import "./styles.scss";
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 function CardProjeto() {
-    return(
+    return (
         <div className="card-projeto">
-         <div className="img-container">
-            
-            <img src={ImgProject} />
-         </div>
-         <div className="title-card">
-            <div className='title-content'>
-            <h3>DG-BARBEARIA</h3>
-                    <p>
-                    Criei este site para oferecer a você uma experiência completa e prática, trazendo todas as informações que você precisa sobre. 
-                    </p>
+            <div className="img-container">
+                <img src={ImgProject} alt="Projeto" />
             </div>
-                    
-
-                    <div className="info-projeto">
-                        <div className="projeto-detalhes">
+            <div className="title-card">
+                <div className='title-content'>
+                    <h3>PROJETO</h3>
+                    <p>
+                        Este é um novo projeto que oferece uma solução inovadora para otimizar processos do dia a dia, proporcionando uma experiência única aos usuários.
+                    </p>
+                </div>
+                <div className="info-projeto">
+                    <div className="projeto-detalhes">
                         <h4>Sobre o Projeto</h4>
-                   
-                    
-
                     </div>
-
                     <div className='projeto-detalhes'>
                         <p>Ano</p>
                         <p>2024</p>
                     </div>
-
                     <div className='projeto-detalhes'>
                         <p>React</p>
                         <p>Front-End</p>
                     </div>
-                    </div>
-                    <div className="projeto-git">
-                        <a href='#'>
-                    
-                         <p>Demonstração </p> 
-
-
-                        </a>
-                        <a href="#"><AttachFileIcon /></a>
-
-                        <a href='#'>
-                    
-                     
-
-
-                  </a>
-                
-                  <a href="#"><GitHubIcon /></a> 
-
-                  </div>
-                      </div>
-                      </div>
-
-
-        
-    )
+                </div>
+                <div className="projeto-git">
+                    <a href="https://demo-link.com" target="_blank" rel="noopener noreferrer">
+                        <p>Demonstração</p>
+                    </a>
+                    <a href="https://" target="_blank" rel="noopener noreferrer">
+                        <AttachFileIcon />
+                    </a>
+                    <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+                        <GitHubIcon />
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default CardProjeto;
--- src/page/index.jsx
+++ src/page/index.jsx
@@ -1,107 +1,101 @@
-import "./style.scss"
-import PhotoBanner  from "../assets/profile.jpg"
+import "./style.scss";
+import PhotoBanner from "../assets/profile.jpg";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import CardProjeto from "../components/CardProjeto";
 import { TypeAnimation } from "react-type-animation";
 
 
-function Home () {
+function Home() {
+
+    const handleScroll = (event, id) => {
+        event.preventDefault();
+        const element = document.getElementById(id);
+        element.scrollIntoView({ behavior: 'smooth' });
+    };
+
     return (
         <div className="home">
             <header>
-            <h2>Felipe Carvalho</h2>
-
-            <nav>
-                <a href="#">Meus Projetos</a>
-                <a href="#">Sobre mim</a>
-                <a href="#">Contato</a>
-            </nav>
+                <h2>Felipe Carvalho</h2>
+                <nav>
+                    <a href="#meus-projetos" onClick={(e) => handleScroll(e, 'meus-projetos')}>Meus Projetos</a>
+                    <a href="#sobre-mim" onClick={(e) => handleScroll(e, 'sobre-mim')}>Sobre mim</a>
+                    <a href="#contato" onClick={(e) => handleScroll(e, 'contato')}>Contato</a>
+                </nav>
             </header>
 
             <main>
                 <div className="section-hero">
                     <div className="BANNER">
                         <h1 className="primeiro-texto">
-                           
-
                             <TypeAnimation
-      sequence={[
-        
-        ' OLÁ, ',
-        1000, 
-        'SOU FELIPE CARVALHO ',
-       
-     
-       
-      ]}
-
-      wrapper="span"
-      speed={50}
-      style={{ fontSize: '63px', display: '' }}
-      repeat={Infinity}
-       />
-
-
+                                sequence={[
+                                    ' OLÁ, ',
+                                    1000,
+                                    'SOU FELIPE CARVALHO ',
+                                ]}
+                                wrapper="span"
+                                speed={50}
+                                style={{ fontSize: '63px', display: '' }}
+                                repeat={Infinity}
+                            />
                         </h1>
                         <p className="paragrafo">
-                        Meu nome é Felipe e sou um entusiasta da programação. Estou animado para compartilhar<br/> minha jornada como programador iniciante.<br/> Sou apaixonado por tecnologia e estou sempre em busca de novos desafios.
+                            Meu nome é Felipe e sou um entusiasta da programação. Estou animado para compartilhar
+                            minha jornada como programador iniciante.
+                            Sou apaixonado por tecnologia e estou sempre em busca de novos desafios.
                         </p>
 
                         <div className="contact-me">
-                            <button>Entre em Contato</button>
-                        <a href="#"><GitHubIcon />
-                        </a>  
-
-                        <a href="#"><WhatsAppIcon /></a>
-
+                        
+                            <a href="https://github.com/Felipegithubf" target="_blank" rel=""><GitHubIcon /></a>
+                            <a href="https://wa.me/21993222068" target="_blank" rel=""><WhatsAppIcon /></a>
                         </div>
-
+                        
                     </div>
-
-                    <img src={PhotoBanner} />
+                    <img src={PhotoBanner} alt="Banner" />
+                   
                 </div>
 
-                <section className="projetos">
-                   <div className="title-projeto">
-                    <h2>Meus Projetos</h2>
+                <section id="meus-projetos" className="projetos">
+                    <div className="title-projeto">
+                        <h2>Meus Projetos</h2>
                     </div>
-
-
-                 
-                   <div className="all-projetos">
-                    <CardProjeto/>
-                    <CardProjeto/>
-                    <CardProjeto/>
-                    <CardProjeto/>
-                    
-                   </div>
-                   </section>
-
-                   
-                
-
-                <section className="sobre-mim">
-                  <h2>
-                    Sobre mim 
-                  </h2>
-                  <div className="descricao">
-                    <h3>Olá, meu nome é Felipe e sou um desenvolvedor front-end iniciante. Minha paixão por tecnologia e design me levou a explorar o mundo do desenvolvimento web, e desde então, fiquei completamente viciado em codar!
-
-                    </h3>
-                    <h4>
-                    Atualmente, estou me aprofundando nas seguintes tecnologias e conceitos:
-
-                    </h4>
-                      <p>Recentemente, comecei a aprender React e estou fascinado pelo poder e pela flexibilidade deste framework. Já criei alguns componentes simples e estou começando a entender como trabalhar com estados, props e o ciclo de vida dos componentes. Estou ansioso para explorar mais sobre hooks e a construção de aplicações mais complexas.</p>
-                  </div>
+                    <div className="all-projetos">
+                        <CardProjeto />
+                        <CardProjeto  />
+                        <CardProjeto  />
+                        <CardProjeto  />
+                    </div>
                 </section>
 
+                
 
+                <section id="sobre-mim" className="sobre-mim">
+                    <h2>Sobre mim</h2>
+                    <div className="descricao">
+                        <h1>Olá, meu nome é Felipe e sou um desenvolvedor front-end iniciante.</h1>
+                        <h3>Minha paixão por tecnologia e design me levou a explorar o mundo do desenvolvimento web, e desde então, fiquei completamente viciado em codar!</h3>
+                        <h4>Atualmente, estou me aprofundando nas seguintes tecnologias e conceitos:</h4>
+                    </div>
+                </section>
+
+                <section id="contato" className="contato">
+                    <h1>Contato</h1>
+                    <form action="mailto:seu-email@dominio.com" method="post" enctype="text/plain">
+                        <label htmlFor="nome">Nome:</label>
+                        <input type="text" id="nome" name="nome" required />
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" id="email" name="email" required />
+                        <label htmlFor="mensagem">Mensagem:</label>
+                        <textarea id="mensagem" name="mensagem" required></textarea>
+                        <button type="submit">Enviar</button>
+                    </form>
+                </section>
             </main>
-            
         </div>
-    )
+    );
 }
 
 export default Home;
